83aeca5c2569cb730869aba83dffe50e19164bfe
lucas bandeira <lucas4workl8tr@gmail.com>
2021-11-14 01:44:03 -0300

    adicionado componentes para renderização

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import { Globalstyle } from "./style/global";
+import {Globalstyle} from "./style/global";
+import {Header} from "./components/Header";
+import {Dashboard} from "./components/Dashboard";
+import Modal from "react-modal";
+import {useState} from "react";
+import {NewTransactionModal} from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export default function App() {
+
+    const [isNewTrasactionModalOpen, setIsNewTrasactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTrasactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTrasactionModalOpen(false);
+    }
+
     return (
-        <div className="App">
-            <h1>Hello World</h1>
+        <>
+            <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+            <Dashboard/>
+            <NewTransactionModal isOpen={isNewTrasactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
+
             <Globalstyle/>
-        </div>
+        </>
     );
 }
